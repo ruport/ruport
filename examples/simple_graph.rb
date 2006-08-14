30760c42004544d08e8b344174e7b03460830a15
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-08-14 22:22:02 +0000

    whoops, didn't mean to commit that example
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk/ruport@143 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- examples/simple_graph.rb
+++ examples/simple_graph.rb
@@ -1,9 +1,9 @@
 require "ruport"
 data = [[1,4,7,9]].to_table(%w[a b c d])
 data[0].tag "snickelfritz"
-puts Ruport::Format.graph(:plugin => :svg, 
-        :data => data, 
-        :options => { :graph_style => :line, 
-                      :graph_title => "Worms on Steriods", 
-                      :show_graph_title => true })
+g = Ruport::Format.graph_object(:plugin => :svg, :data => data)
+g.options = { :graph_style => :line, 
+               :graph_title => "Worms on Steriods", 
+               :show_graph_title => true }
 
+puts g.render
