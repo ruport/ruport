e95ad0f5763e5bf0c5d21f1a6b876e3624b237a9
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-08-24 18:36:51 +0000

    Added more Collection tests.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@171 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- test/test_collection.rb
+++ test/test_collection.rb
@@ -5,6 +5,11 @@ class TestCollection < Test::Unit::TestCase
 
 	def setup
 		@ghosts = Ruport::Data::Collection.new %w[inky blinky clyde]
+		@ghost_list = [["inky", "blue"],["blinky","red"],["clyde","orange"]]
+		@ghost_records = @ghost_list.map {|x| Ruport::Data::Record.new x }
+		@ghost_collection = Ruport::Data::Collection.new @ghost_records
+		@ghost_table = Ruport::Data::Table.new :data => @ghost_list
+		@ghost_set = Ruport::Data::Set.new :data => @ghost_list
 	end
 
 	def test_size
@@ -13,5 +18,22 @@ class TestCollection < Test::Unit::TestCase
 		assert_equal @ghosts.length, @ghosts.size
 		assert_equal @ghosts.length, @ghosts.data.size
 	end
+
+  def test_to_table
+    assert_equal @ghost_table, @ghost_collection.to_table
+  end
+  
+  def test_to_set
+    assert_equal @ghost_set, @ghost_collection.to_set
+  end
+
+  def test_to_csv
+    assert_equal "inky,blue\nblinky,red\nclyde,orange\n", 
+                 @ghost_collection.to_csv
+  end
+  
+  def test_as_csv
+    assert_equal @ghost_collection.to_csv, @ghost_collection.as(:csv)
+  end
 
 end
