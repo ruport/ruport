61447024f1e27de76dd85ff5e4772ebed3915169
dinko <dinko@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-10-20 12:51:55 +0000

    simple fix for ticket:92. will expand after 0.6.0.  Also, added missing attempt.rb file to stable
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@295 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/data/groupable.rb
+++ lib/ruport/data/groupable.rb
@@ -1,7 +1,11 @@
 module Ruport::Data
   module Groupable
     def group_by_tag
-      
+      r_tags = data.map {|row| row.tags}.flatten.uniq
+      d = r_tags.map do |t| 
+	 select {|row| row.tags.include? t }.to_table(column_names)      
+      end
+      Record.new d, :attributes => r_tags
     end
   end
 end
