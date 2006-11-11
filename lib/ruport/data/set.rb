b5afeac7acf8acc6ac5eebfde092984f72585c43
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-11-11 21:19:11 +0000

    Update api docs for Set.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@320 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/data/set.rb
+++ lib/ruport/data/set.rb
@@ -7,6 +7,7 @@ require 'set'
 
 module Ruport::Data
   
+  #
   # This class is one of the core classes for building and working with data 
   # in Ruport. The idea is to get your data into a standard form, regardless 
   # of its source (a database, manual arrays, ActiveRecord, CSVs, etc.).
@@ -17,19 +18,29 @@ module Ruport::Data
   #
   # Once your data is in a Ruport::Data::Set object, it can be manipulated
   # to suit your needs, then used to build a report.
+  #
   class Set < Collection
     
-    # Creates a new set containing the elements of options[:data].
+    #
+    # Creates a new Set containing the elements of <tt>options[:data]</tt>.
+    #
+    # Example:
     #
     #   Set.new :data => [%w[one two three] %w[1 2 3] %w[I II III]]
+    #
     def initialize(options={})
       @data = ::Set.new
       options[:data].each {|e| self << e} if options[:data]
     end
     
-    # Adds the given object to the set and returns self.
+    #
+    # Adds the given object to the Set and returns self. 
+    #
+    # Example:
+    #
     #   set = Set.new :data => [%w[one two three]]
     #   set << [5,6,7]
+    #
     def add(other)
       case other
         when Record 
@@ -41,14 +52,18 @@ module Ruport::Data
     end
     alias_method :<<, :add
     
-    # Produces a shallow copy of the set: the same data elements are 
-    # referenced by both the old and new sets.
+    #
+    # Produces a shallow copy of the Set: the same data elements are 
+    # referenced by both the old and new Sets.
+    #
+    # Example:
     #
     #   set = Set.new :data => [%w[one two three]]
     #   set2 = set.dup
     #   set == set2 #=> true
     #   set << [8,9,10]
     #   set == set2 #=> false
+    #
     def dup
      a =  self.class.new(:data=>@data)
      a.tags = tags.dup
@@ -56,62 +71,80 @@ module Ruport::Data
     end
     alias_method :clone, :dup
 
-    # Equality. Two sets are equal if they contain the same set of objects.
+    #
+    # Two Sets are equal if they contain the same set of objects.
+    # 
+    # Example:
     #   s1 = Set.new :data => [[1,2,3]]
     #   s2 = Set.new :data => [[1,2,3]]
     #   s1 == s2 #=> true
+    #
     def ==(other)
       @data == other.data
     end
     
-    # Union. Returns a new set containing the union of the objects contained in
-    # the two sets.
+    # Returns a new Set containing the all of the objects contained in either 
+    # of the two Sets.
+    #
+    # Example:
     #
     #   s1 = Set.new :data => [[1,2,3]]
     #   s2 = Set.new :data => [[4,5,6]]
     #   s3 = s1 | s2
     #   s4 = Set.new :data => [[1,2,3], [4,5,6]]
     #   s3 == s4 #=> true
+    #
     def |(other)
       Set.new :data => (@data | other.data)
     end
     alias_method :union, :|
     alias_method :+, :|
     
-    # Intersection. Returns a new set containing the objects common to the two
-    # sets.
+    #
+    # Returns a new Set containing the objects common to the two Sets.
+    #
+    # Example:
     #
     #   s1 = Set.new :data => [%w[a b c],[1,2,3]]
     #   s2 = Set.new :data => [%w[a b c],[4,5,6]]
     #   s3 = s1 & s2
     #   s4 = Set.new :data => [%w[a b c]]
     #   s3 == s4 #=> true
+    #
     def &(other)
       Set.new :data => (@data & other.data)
     end
     alias_method :intersection, :&
     
-    # Difference. Returns a new set containing those objects present in this
-    # set but not the other.
+    #
+    # Returns a new Set containing those objects present in this Set but not 
+    # the other.
+    #
+    # Example:
     #
     #   s1 = Set.new :data => [%w[a b c],[1,2,3]]
     #   s2 = Set.new :data => [%w[a b c],[4,5,6]]
     #   s3 = s1 - s2 
     #   s4 = Set.new :data => [[1, 2, 3]]
     #   s3 == s4 #=> true 
+    #
     def -(other)
       Set.new :data => (@data - other.data)
     end
     alias_method :difference, :-
 
-    # Exclusion. Returns a new set containing those objects in this set or the
-    # other set but not in both.
+    #
+    # Returns a new Set containing those objects that are either in this Set 
+    # or the other Set but not in both.
+    #
+    # Example:
     # 
     #   s1 = Set.new :data => [%w[a b c],[1,2,3]]
     #   s2 = Set.new :data => [%w[a b c],[4,5,6]]
     #   s3 = s1 ^ s2
     #   s4 = Set.new :data => [[1, 2, 3],[4,5,6]]
     #   s3 == s4 #=> true   
+    #
     def ^(other)
       Set.new :data => (@data ^ other.data)
     end
