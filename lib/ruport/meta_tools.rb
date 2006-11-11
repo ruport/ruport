8cfabb0fa7ef887d9ec4a05caddef97e16419549
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-11-11 22:36:31 +0000

    Update the api docs for MetaTools.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@324 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/meta_tools.rb
+++ lib/ruport/meta_tools.rb
@@ -1,11 +1,15 @@
+# Gets raised if you try to add the same action to an object more than once. 
 class ActionAlreadyDefinedError < RuntimeError; end
+
 module Ruport
+  #
   # This module provides a few tools for doing some manipulations of the
-  # eigenclass on an object.  These are used in the implementation of Ruport's
-  # formatting system, and might be helpful for other things.
+  # singleton class of an object.  These are used in the implementation of 
+  # Ruport's formatting system, and might be useful for other things.
   #
   module MetaTools
-    # Allows you to define an attribute accessor on the singleton_class.
+    #
+    # Allows you to define an attribute accessor on the singleton class.
     #
     # Example:
     # 
@@ -14,21 +18,27 @@ module Ruport
     #     attribute :foo
     #  end
     #
-    #  A.foo #=> nil
-    #  A.foo = 7 #=> 7 
+    #  A.foo      #=> nil
+    #  A.foo = 7  #=> 7 
+    #
     def attribute(sym,value = nil)
       singleton_class.send(:attr_accessor, sym )
       self.send("#{sym}=",value)
     end
 
-    # Same as attribute, but takes an array of attributes
     #
-    # e.g. attributes [:foo,:bar,:baz]
+    # Same as <tt>attribute</tt>, but takes an Array of attributes.
+    #
+    # Example:
+    # 
+    #   attributes [:foo,:bar,:baz]
+    #
     def attributes(syms)
       syms.each { |s| attribute s }
     end
     
-    # Allows you to define a method on the singleton_class
+    #
+    # Allows you to define a method on the singleton class.
     #
     # Example:
     #
@@ -37,15 +47,17 @@ module Ruport
     #     action(:bar) { |x| x + 1 }
     #   end
     #
-    #   A.bar(3) #=> 4
+    #   A.bar(3)  #=> 4
+    #
     def action(name,&block)
       raise ActionAlreadyDefinedError if respond_to? name
       singleton_class.send(:define_method, name, &block)
     end
+    
   end
 end
 
 class Module
-    # provides the singleton_class object
-    def singleton_class; (class << self; self; end); end
+  # Provides the singleton_class object.
+  def singleton_class; (class << self; self; end); end
 end
