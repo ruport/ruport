958b80bf7cf05b7739d691b4a4a77ff1338f5b8e
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-11-11 22:13:03 +0000

    Update api docs for Taggable.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@322 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/data/taggable.rb
+++ lib/ruport/data/taggable.rb
@@ -5,38 +5,64 @@
 # Copyright 2006 by respective content owners, all rights reserved.
 module Ruport::Data
   
-  # This module provides a simple mechanism for tagging arbitrary objects.  This
-  # provides the necessary methods to set and retrieve tags which can consist of
-  # any Ruby object.  This is used by Data::Record and the Ruport Data
-  # Collections.
+  #
+  # This module provides a simple mechanism for tagging arbitrary objects.  
+  # It provides the necessary methods to set and retrieve tags which can 
+  # consist of any Ruby object.
+  #
   module Taggable
 
-    # Adds a tag to the object 
+    #
+    # Adds a tag to the object.
+    #
+    # Example:
+    #
     #   taggable_obj.tag :spiffy
+    #
     def tag(tag_name)
       tags << tag_name unless has_tag? tag_name
     end
     
-    # Removes a tag
+    #
+    # Removes a tag from the object.
+    #
+    # Example:
+    #
     #   taggable_obj.delete_tag :not_so_spiffy
+    #
     def delete_tag(tag_name)
       tags.delete tag_name
     end
   
-    # Checks to see if a tag is present
+    #
+    # Checks to see if a tag is present.
+    #
+    # Example:
+    #
     #   taggable_obj.has_tag? :spiffy #=> true
+    #
     def has_tag?(tag_name)
       tags.include? tag_name
     end
   
-    # Returns an array of tags.
+    # 
+    # Returns an Array of the object's tags.
+    #
+    # Example:
+    #
     #   taggable_obj.tags #=> [:spiffy, :kind_of_spiffy]
+    #
     def tags
       @ruport_tags ||= []
     end
    
-    # Sets the tags to some array
+    # 
+    # Sets the tags to some Array.
+    #
+    # Example:
+    #
     #   taggable_obj.tags = [:really_dang_spiffy, :the_most_spiffy]
+    #
     def tags=(tags_list)
       @ruport_tags = tags_list
     end
