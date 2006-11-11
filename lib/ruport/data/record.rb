bee9efaa03aeef6a2da705275dddb29094f03372
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-11-11 21:07:48 +0000

    Update api docs for Record.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@319 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/data/record.rb
+++ lib/ruport/data/record.rb
@@ -5,21 +5,26 @@
 # Copyright 2006 by respective content owners, all rights reserved.
 module Ruport::Data
 
-  # Data::Records are the work horse of Ruport's Data model.  These can behave
-  # as array like, hash like, or struct like objects.  They are used as the base
-  # record for both Tables and Sets in Ruport.
+  #
+  # Data::Records are the work horse of Ruport's data model. These can behave
+  # as Array-like, Hash-like, or Struct-like objects.  They are used as the 
+  # base element in both Tables and Sets.
+  #
   class Record
     require "forwardable"
     extend Forwardable
     include Enumerable
     include Taggable
 
-    # Creates a new Record object.  If the <tt>:attributes</tt> keyword is
-    # specified, Hash-like and Struct-like access will be enabled.  Otherwise,
-    # Record elements may be accessed ordinally, like an Array.
     # 
-    # Records accept either Hashes or Arrays as their data.
+    # Creates a new Record object.  If the <tt>:attributes</tt> 
+    # keyword is specified, Hash-like and Struct-like 
+    # access will be enabled.  Otherwise, Record elements may be 
+    # accessed ordinally, like an Array.
+    # 
+    # A Record can accept either a Hash or an Array as its <tt>data</tt>.
     #
+    # Examples:
     #   a = Record.new [1,2,3]
     #   a[1] #=> 2
     #
@@ -37,6 +42,7 @@ module Ruport::Data
     #   b[1]   #=> ? (without attributes, you cannot rely on order)
     #   b['a'] #=> 1
     #   b.c    #=> 3
+    #
     def initialize(data,options={})
       if data.kind_of?(Hash)
         if options[:attributes]
@@ -51,18 +57,19 @@ module Ruport::Data
       end
     end
     
-    # The underlying data which is being stored in the record
+    # The underlying <tt>data</tt> which is being stored in the record.
     attr_reader :data
     
     def_delegators :@data,:each, :length
   
-    # Allows either array or hash_like indexing
+    #
+    # Allows either Array or Hash-like indexing.
+    #
+    # Examples:
     #
     #   my_record[1] 
     #   my_record["foo"]
     #
-    # Also, this provides a feature via method_missing which allows
-    #   my_record.foo
     def [](index)
       if index.kind_of? Integer
         raise "Invalid index" unless index < @data.length
@@ -75,13 +82,14 @@ module Ruport::Data
     end
 
 
-    # Allows setting a value at an index
-    #  
+    # 
+    # Allows setting a <tt>value</tt> at an <tt>index</tt>.
+    # 
+    # Examples:
+    #
     #    my_record[1] = "foo" 
     #    my_record["bar"] = "baz"
     #
-    # And via method_missing
-    #    my_record.ghost = "blinky"
     def []=(index, value)
       if index.kind_of? Integer
         raise "Invalid index" unless index < @data.length
@@ -93,9 +101,10 @@ module Ruport::Data
       end
     end
 
-
-    # If attributes and data are equivalent, then == evaluates to true.
-    # Otherwise, == returns false
+    #
+    # If <tt>attributes</tt> and <tt>data</tt> are equivalent, then 
+    # <tt>==</tt> evaluates to true. Otherwise, <tt>==</tt> returns false.
+    #
     def ==(other)
       return false if @attributes && !other.attributes
       return false if other.attributes && !@attributes
@@ -104,41 +113,61 @@ module Ruport::Data
 
     alias_method :eql?, :==
    
-    # Makes an array out of the data wrapped by Record
+    #
+    # Converts a Record into an Array.
+    #
+    # Example:
     #
     #   a = Data::Record.new([1,2],:attributes => %w[a b])
     #   a.to_a #=> [1,2]
+    #
     def to_a; @data.dup; end
-   
-    # Makes a hash out of the data wrapped by Record
-    # Only works if attributes are specified
+    
+    #
+    # Converts a Record into a Hash. This only works if <tt>attributes</tt> 
+    # are specified in the Record.
+    #
+    # Example:
     #
     #   a = Data::Record.new([1,2],:attributes => %w[a b])
     #   a.to_h #=> {"a" => 1, "b" => 2}
     def to_h; Hash[*@attributes.zip(data).flatten] end
   
-    # Returns a copy of the list of attribute names associated with this Record.
+    #
+    # Returns a copy of the <tt>attributes</tt> from this Record.
+    #
+    # Example:
     #
     #   a = Data::Record.new([1,2],:attributes => %w[a b])
     #   a.attributes #=> ["a","b"]
-    def attributes; @attributes.map { |a| a.to_s } ; end
+    #
+    def attributes; @attributes.map { |a| a.to_s }; end
 
-    # Sets the attribute list for this Record
+    #
+    # Sets the <tt>attribute</tt> list for this Record.
+    #
+    # Example:
     #
     #   my_record.attributes = %w[foo bar baz]
+    #
     attr_writer :attributes
+
+    #
     # Allows you to change the order of or reduce the number of columns in a
-    # Record.  Example:
+    # Record.  
+    #
+    # Example:
     #
     #   a = Data::Record.new([1,2,3,4],:attributes => %w[a b c d])
     #   b = a.reorder("a","d","b")
     #   b.attributes #=> ["a","d","b"]
     #   b.data #=> [1,4,2]
+    #
     def reorder(*indices)
       dup.reorder!(*indices)
     end
     
-    # Same as Record#reorder but is destructive
+    # Same as Record#reorder but modifies its reciever in place.
     def reorder!(*indices)
       indices = reorder_data!(*indices)
       if @attributes
@@ -150,7 +179,7 @@ module Ruport::Data
       end; self
     end
 
-    def reorder_data!(*indices)
+    def reorder_data!(*indices) # :nodoc:
       indices = indices[0] if indices[0].kind_of?(Array) 
       indices.each do |i| 
         self[i] rescue raise ArgumentError, 
@@ -161,7 +190,7 @@ module Ruport::Data
     end
 
     
-    # Makes a fresh copy of the Record
+    # Makes a fresh copy of the Record.
     def dup
       copy = self.class.new(@data,:attributes => attributes)
       copy.tags = self.tags.dup
@@ -171,15 +200,24 @@ module Ruport::Data
     #FIXME: This does not take into account frozen / tainted state
     alias_method :clone, :dup
     
-    # provides a unique hash value
+    #
+    # Provides a unique hash value. If a Record contains the same data and
+    # attributes as another Record, they will hash to the same value, even if
+    # they are not the same object. This is similar to the way Array works, 
+    # but different from Hash and other objects.
+    #
     def hash
       (attributes.to_a + data.to_a).hash
     end
 
-    # provides accessor style methods for attribute access, Example
+    #
+    # Provides accessor style methods for attribute access.
+    #
+    # Example:
     #
     #   my_record.foo = 2
     #   my_record.foo #=> 2
+    #
     def method_missing(id,*args)
       id = id.to_s.gsub(/=$/,"")
       if attributes.include?(id)
