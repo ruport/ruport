cb6accb6160640bbd0faef9ed07c9e8a4a69d4ed
dudley <dudley@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2006-11-11 20:26:02 +0000

    Merge Config api doc changes from stable.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@315 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/config.rb
+++ lib/ruport/config.rb
@@ -11,137 +11,165 @@
 #
 require "ostruct"
 module Ruport
+  #
   # This class serves as the configuration system for Ruport.
-  # It's functionality is implemented through Config::method_missing
   #  
-  # source :default and mailer :default will become the fallback values if one
-  # is not specified in Report::Mailer or Query, but you may define as many
-  # sources as you like and switch between them later.
+  # The source and mailer defined as <tt>:default</tt> will become the 
+  # fallback values if you don't specify one in <tt>Report::Mailer</tt> or 
+  # <tt>Query</tt>, but you may define as many sources as you like and switch 
+  # between them later.
   #
-  # An example config file is shown below:
+  # The most common way to access your application configuration is through 
+  # the <tt>Ruport.configure</tt> method, like this:
   #   
-  #   # password is optional, dsn may omit hostname for localhost
-  #   Ruport::Config.source :default,
-  #   :dsn => "dbi:mysql:somedb:db.blixy.org",
-  #   :user => "root", :password => "chunky_bacon"
-  #   
-  #   # :password, :port, and :auth_type are optional. :port defaults to 25 and
-  #   # :auth_type defaults to :plain.  For more information, see the source
-  #   # of Report::Mailer#select_mailer
-  #   Ruport::Config.mailer :default,
-  #   :host => "mail.chunkybacon.org", :address => "chunky@bacon.net",
-  #   :user => "cartoon", :password => "fox", :port => 25, :auth_type => :login
-  #   
-  #   # optional, if specifed, Ruport#complain will report to it
-  #   Ruport::Config.log_file 'foo.log'
+  #   Ruport.configure do |config|
   #
-  #   # optional, if enabled, will force :log_only complaint calls to
-  #   # print to secondary output ($sterr by default).
-  #   # call Ruport::Config.disable_paranoia to disable
-  #   Ruport::Config.enable_paranoia
+  #     config.log_file 'foo.log'
+  #     config.enable_paranoia
   #
-  # Alternatively, this configuration could be done by opening the class:
+  #     config.source :default,
+  #                   :dsn => "dbi:mysql:somedb:db.blixy.org",
+  #                   :user => "root", 
+  #                   :password => "chunky_bacon"
+  #   
+  #     config.mailer :default,
+  #                   :host => "mail.chunkybacon.org", 
+  #                   :address => "chunky@bacon.net",
+  #                   :user => "cartoon", 
+  #                   :password => "fox", 
+  #                   :port => 25, 
+  #                   :auth_type => :login
+  #
+  #   end
+  #
+  # You can accomplish the same thing by opening the class directly:
+  #
   #   class Ruport::Config
   #
-  #     source :default, :dsn => "dbi:mysql:some_db", :user => "root"
+  #     source :default, 
+  #            :dsn => "dbi:mysql:some_db", 
+  #            :user => "root"
   #     
-  #     mailer :default, :host => "mail.iheartwhy.com", 
-  #     :address => "sandal@ruby-harmonix.net", :user => "sandal",
-  #     :password => "abc123"
+  #     mailer :default, 
+  #            :host => "mail.iheartwhy.com", 
+  #            :address => "sandal@ruby-harmonix.net", 
+  #            :user => "sandal",
+  #            :password => "abc123"
   #     
   #     logfile 'foo.log'
   #
   #   end
   #
-  # Saving this config information into a file and then requiring it can allow
-  # you share configurations between Ruport applications.
+  # Saving this config information into a file and then requiring it allows
+  # you to share configurations between Ruport applications. 
   #
   module Config
     module_function
-
-
-    # create or retrieve a database source configuration.
+    # 
+    # :call-seq:
+    #   source(source_name, options)
     #
-    # setting a source
+    # Creates or retrieves a database source configuration. Available options
+    # are:
+    # <b><tt>:user</tt></b>::       The user used to connect to the database.
+    # <b><tt>:password</tt></b>::   The password to use to connect to the 
+    #                               database (optional).
+    # <b><tt>:dsn</tt></b>::        The dsn string that dbi will use to 
+    #                               access the database.
     #
-    #   source :default, :user => "root", :password => "clyde",
+    # Example (setting a source): 
+    #   source :default, :user => "root", 
+    #                    :password => "clyde",
     #                    :dsn  => "dbi:mysql:blinkybase"
     #
-    # retrieving a source
-    #
+    # Example (retrieving a source):
     #   db = source(:default) #=> <OpenStruct ..>
-    #   db.dsn #=> "dbi:mysql:blinkybase"
+    #   db.dsn                #=> "dbi:mysql:blinkybase"
+    #
     def source(*args) 
       return sources[args.first] if args.length == 1
       sources[args.first] = OpenStruct.new(*args[1..-1])
       check_source(sources[args.first],args.first)
     end
 
-    # create or retrieve a mailer configuration
+    # 
+    # :call-seq:
+    #   mailer(mailer_name, options)
     #
-    # creating a mailer config
-    #
+    # Creates or retrieves a mailer configuration. Available options:
+    # <b><tt>:host</tt></b>::         The SMTP host to use.
+    # <b><tt>:address</tt></b>::      The email address to send to.
+    # <b><tt>:user</tt></b>::         The username to use on the SMTP server
+    # <b><tt>:password</tt></b>::     The password to use on the SMTP server. 
+    #                                 Optional.
+    # <b><tt>:port</tt></b>::         The SMTP port to use. Optional, defaults
+    #                                 to 25.
+    # <b><tt>:auth_type</tt></b>::    SMTP authorization method. Optional, 
+    #                                 defaults to <tt>:plain</tt>.
+    # <b><tt>:mail_klass</tt></b>::   If you don't want to use the default 
+    #                                 <tt>MailFactory</tt> object, you can 
+    #                                 pass another mailer to use here.
+    #                               
+    # Example (creating a mailer config):
     #   mailer :alternate, :host => "mail.test.com", 
     #                      :address => "test@test.com",
-    #                      :user => "test", :password => "blinky"
+    #                      :user => "test", 
+    #                      :password => "blinky"
     #                      :auth_type => :cram_md5
     #
-    # retreiving a mailer config
+    # Example (retreiving a mailer config):
+    #   mail_conf = mailer(:alternate)  #=> <OpenStruct ..>
+    #   mail_conf.address               #=> test@test.com
     #
-    #   mail_conf = mailer(:alternate) #=> <OpenStruct ..>
-    #   mail_conf.address #=> test@test.com
     def mailer(*args)
       return mailers[args.first] if args.length == 1
       mailers[args.first] = OpenStruct.new(*args[1..-1])
       check_mailer(mailers[args.first],args.first)
     end
 
-
-    # Sets the logger to use the specified file.
-    #
-    #   log_file "foo.log"
+    # The file that <tt>Ruport.log()</tt> will write to.
     def log_file(file)
       @logger = Logger.new(file)
     end
     
-    # Same as Config.log_file, but accessor style
+    # Same as <tt>Config.log_file</tt>, but accessor style.
     def log_file=(file)
       log_file(file)
     end
 
-    # Returns the source which is labeled :default
+    # Alias for <tt>sources[:default]</tt>.
     def default_source
       sources[:default]
     end
 
-    # Returns the mailer which is labeled :default
+    # Alias for <tt>mailers[:default]</tt>.
     def default_mailer
       mailers[:default]
     end
 
-    # Returns an array of database source configs
+    # Returns all <tt>source</tt>s defined in this <tt>Config</tt>.
     def sources; @sources ||= {}; end
 
-    # Returns an array of mailer configs
+    # Returns all the <tt>mailer</tt>s defined in this <tt>Config</tt>.
     def mailers; @mailers ||= {}; end
 
-    # Returns the currently active logger
+    # Returns the currently active logger.
     def logger; @logger; end
 
-    # Forces all messages marked :log_only to surface
+    # Forces all messages marked <tt>:log_only</tt> to print anyway.
     def enable_paranoia; @paranoid = true; end
 
-    # Disables the printing of :log_only messages to STDERR
+    # Disables the printing of <tt>:log_only</tt> messages.
     def disable_paranoia; @paranoid = false; end
 
-    # Sets paranoid status
+    # Sets paranoid status.
     def paranoid=(val); @paranoid = val; end
 
-    # Checks to see if paranoia is enabled
+    # Checks to see if paranoia is enabled.
     def paranoid?; !!@paranoid; end
     
-    # Verifies that you have provided a DSN for your source
-    def check_source(settings,label)
+    # Verifies that you have provided a DSN for your source.
+    def check_source(settings,label) # :nodoc:
       unless settings.dsn
         Ruport.complain( 
           "Missing DSN for source #{label}!",
@@ -151,8 +179,8 @@ module Ruport
       end
     end
 
-    # Verifies that you have provided a host for your mailer
-    def check_mailer(settings, label)
+    # Verifies that you have provided a host for your mailer.
+    def check_mailer(settings, label) # :nodoc:
       unless settings.host
         Ruport.complain(
           "Missing host for mailer #{label}",
