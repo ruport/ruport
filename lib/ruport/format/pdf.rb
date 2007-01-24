22c1cb068c8d1e3a88077af2b115c075a364d10c
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-01-24 17:18:03 +0000

    patchs from jh
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@462 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/format/pdf.rb
+++ lib/ruport/format/pdf.rb
@@ -46,27 +46,115 @@ module Ruport::Format
       pdf_writer.text(*args)
     end
        
+    # - if the image is bigger than the box, it will be scaled down until it fits
+    # - if the image is smaller than the box it's won't be resized
+    #
+    # arguments:
+    # - x: left bound of box
+    # - y: bottom bound of box
+    # - width: width of box
+    # - height: height of box
+    def center_image_in_box(path, x, y, width, height)
+      info = ::PDF::Writer::Graphics::ImageInfo.new(File.read(path))
+
+      # if the image is larger than the requested box, prepare to
+      # scale it down
+      fits = !(info.width > width || info.height > height)
+
+      # setup initial sizes for the image. These will be reduced as necesary
+      img_width = info.width
+      img_height = info.height
+      img_ratio = info.height.to_f / info.width.to_f
+
+      # reduce the size of the image until it fits into the requested box
+      until fits
+        img_width -= 1
+        img_height = img_width * img_ratio
+        fits = true if img_width < width && img_height < height
+      end
+
+      # if the width of the image is less than the requested box, calculate
+      # the white space buffer
+      if img_width < width
+        white_space = width - img_width
+        x = x + (white_space / 2)
+      end
+
+      # if the height of the image is less than the requested box, calculate
+      # the white space buffer
+      if img_height < height
+        white_space = height - img_height
+        y = y + (white_space / 2)
+      end
+
+      pdf_writer.add_image_from_file(path, x, y, img_width, img_height) 
+    end
+
+    # Draws some text on the canvas, surrounded by a box with rounded corners
+    #
     def rounded_text_box(text)
        opts = OpenStruct.new
        yield(opts)
        
+       # resize the text automatically to ensure it isn't wider than the box
        loop do
          sz = pdf_writer.text_width( text, opts.font_size )
          opts.x + sz > opts.x + opts.width or break
          opts.font_size -= 1
        end
 
+       # save the drawing state (colors, etc) so we can restore it later
+       pdf_writer.save_state
+
+       # draw our box
        pdf_writer.fill_color(opts.fill_color || Color::RGB::White)
        pdf_writer.stroke_color(opts.stroke_color || Color::RGB::Black)
        pdf_writer.rounded_rectangle( opts.x, opts.y, 
-                                     opts.width, opts.height, opts.radius).fill_stroke
-       pdf_writer.fill_color(Color::RGB::Black)
-       pdf_writer.stroke_color(Color::RGB::Black)     
-       pdf_writer.y = opts.y
+                                     opts.width, opts.height, 
+                                     opts.radius).fill_stroke
+
+       # if a heading for this box has been specified
+       if opts.heading
+         pdf_writer.line( opts.x, opts.y - 20, 
+                          opts.x + opts.width, opts.y - 20).stroke
+         pdf_writer.fill_color(Color::RGB::Black)
+         move_cursor_to(opts.y - 3)
+         pdf_writer.text("<b>#{opts.heading}</b>", 
+           :absolute_left => opts.x, :absolute_right => opts.x + opts.width,
+           :justification => :center, :font_size => opts.font_size)
+       end
+
+       # restore the original colors
+       pdf_writer.restore_state
+
+       # move our y cursor into position, write the text, then move the cursor
+       # to be just below the box
+       pdf_writer.y = opts.heading ? opts.y - 20 : opts.y
+
        add_text( text,  :absolute_left  => opts.x,
                         :absolute_right => opts.x + opts.width,
                         :justification => opts.justification || :center,
                         :font_size => opts.font_size )     
+
+       pdf_writer.y = opts.y - opts.height
+    end
+
+    # adds an image to every page. The color and size won't be modified,
+    # but it will be centered.
+    #
+    def watermark(imgpath)
+      x = pdf_writer.absolute_left_margin
+      y = pdf_writer.absolute_bottom_margin
+      width = pdf_writer.absolute_right_margin - x
+      height = pdf_writer.absolute_top_margin - y
+
+      pdf_writer.open_object do |wm|
+        pdf_writer.save_state
+        center_image_in_box(imgpath, x, y, width, height)
+        pdf_writer.restore_state
+        pdf_writer.close_object
+        pdf_writer.add_object(wm, :all_pages)
+      end
     end
 
     def move_cursor(n) 
