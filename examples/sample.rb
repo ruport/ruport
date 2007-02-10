0fd344531309be68ea0a368626fee82e30d85f84
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-02-10 19:37:33 +0000

    new stuff
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@531 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

---
+++ examples/sample.rb
@@ -0,0 +1,15 @@
+require "lib/init"
+class Sample < Ruport::Report
+
+  renders_with Ruport::Renderer::Table
+  
+  def generate
+    Table(%w[a b c]) << [1,2,3] << [4,5,6]
+  end
+
+end
+
+if __FILE__ == $0
+  report = Sample.new(:csv)
+  puts report.run
+end
