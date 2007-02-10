35bf9aac75cb5f8d930587bd1557f60afa89a58f
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-02-10 16:05:56 +0000

    line graph example that found a bug
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@525 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

---
+++ examples/line_graph.rb
@@ -0,0 +1,25 @@
+require "ruport"
+table = Table(%w[jan feb mar apr may jun jul]) do |t|
+  t << [5,7,9,12,14,16,18]
+  t << [21,3,8,19,13,15,1]
+end
+
+results = Ruport::Renderer::Graph.
+  render_svg( :width => 700, :height => 500, :style => :line,
+              :title => "Simple Line Graph" ) { |r| r.data = table }
+
+File.open("line_graph.svg","w") { |f| f << results }
+
+PAGE = <<-END_HTML
+<html>
+  <body>
+  <embed
+  src="line_graph.svg"
+  width="700"
+  height="500"
+  />
+</body>
+<html>
+END_HTML
+
+File.open("line_graph.html","w") { |f| f << PAGE }
