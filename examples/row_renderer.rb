4d15472171cf1bc48dc397a0dc4fcc740b51ecd5
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-02-23 23:24:18 +0000

    updated to use mike's formatter
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@554 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- examples/row_renderer.rb
+++ examples/row_renderer.rb
@@ -1,27 +1,9 @@
 require "ruport"
 
-class RowRenderer < Ruport::Renderer
+puts "<html><body><table>"
 
-  include Ruport::Renderer::Helpers
-
-  stage :row 
-  required_option :record
-  option :class_str
-
+Ruport::Data::Table.load("example.csv", :has_names => false) do |s,r|
+  Ruport::Data::Record.new(r).as(:html, :record => r, :io => STDOUT)
 end
 
-class HTML < Ruport::Format::HTML
-  
-  RowRenderer.add_format self, :html
-
-  def build_row
-    build_html_row(options.record,options.class_str)
-  end
-
-end
-
-Ruport::Data::Table.load("hygjan2007.csv", :has_names => false) do |s,r|
-  puts "<html><body><table>"
-  RowRenderer.render_html(:record => r, :io => STDOUT)
-  puts "</table></body></html>"
-end
+puts "</table></body></html>"
