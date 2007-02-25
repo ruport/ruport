ed2183e40a3a188dff9881af846c41e1e1bd6f5e
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-02-25 20:25:33 +0000

    Uses data accessor now
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/trunk@558 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- examples/row_renderer.rb
+++ examples/row_renderer.rb
@@ -3,7 +3,5 @@ require "ruport"
 puts "<html><body><table>"
 
 Ruport::Data::Table.load("example.csv", :has_names => false) do |s,r|
-  Ruport::Data::Record.new(r).as(:html, :record => r, :io => STDOUT)
+  Ruport::Data::Record.new(r).as(:html, :data => r, :io => STDOUT)
 end
-
-puts "</table></body></html>"
