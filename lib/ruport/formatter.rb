2583ebb379ea9b3574d8ab96bca7e636b0481c1e
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-04-01 16:09:44 +0000

    Doc patch and fix for #191 (untested
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@666 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/formatter.rb
+++ lib/ruport/formatter.rb
@@ -9,22 +9,48 @@ module Ruport
     
     module RenderingTools
 
+      # Iterates through <tt>data</tt> and passes
+      # each row to render_row with the given options
       def render_data_by_row(options={},&block)
         data.each do |r|
           render_row(r,options,&block)
         end
       end
 
+      # Uses Renderer::Row to render the Row object with the
+      # given options.
+      #
+      # Sets the :io attribute by default to the existing formatter's
+      # output object.
       def render_row(row,options={},&block)
         render_helper(Renderer::Row,row,options,&block)
       end
 
+      # Uses Renderer::Table to render the Table object with the
+      # given options.
+      #
+      # Sets the :io attribute by default to the existing formatter's
+      # output object.
       def render_table(table,options={},&block)
         render_helper(Renderer::Table,table,options,&block)
       end
 
+      # Uses Renderer::Group to render the Group object with the
+      # given options.
+      #
+      # Sets the :io attribute by default to the existing formatter's
+      # output object.
       def render_group(group,options={},&block)
         render_helper(Renderer::Group,group,options,&block)
+      end
+
+      # Uses Renderer::Grouping to render the Grouping object with the
+      # given options.
+      #
+      # Sets the :io attribute by default to the existing formatter's
+      # output object.
+      def render_grouping(group,options={},&block)
+        render_helper(Renderer::Grouping,group,options,&block)
       end
 
       private
@@ -40,16 +66,20 @@ module Ruport
 
     module OptionAccessors
 
-
       module ClassMethods
-        def opt_reader(*opts)
+
+        # allows the options specified to be accessed directly
+        # 
+        #   opt_reader :something
+        #   something == options.something #=> true
+        def opt_reader(*opts) 
           require "forwardable"
           extend Forwardable
           opts.each { |o| def_delegator :@options, o }
         end
       end
 
-      def self.included(base)
+      def self.included(base) #:nodoc:
         base.extend(ClassMethods)
       end
     end
@@ -61,6 +91,12 @@ module Ruport
     attr_accessor :data
     attr_accessor :format
 
+    # Registers the formatter with one or more Renderers
+    #
+    #   renders :pdf, :for => MyRenderer
+    #   renders [:csv,:html], :for => YourRenderer
+    #   render :text, :for => [MyRenderer,YourRenderer]
+    #
     def self.renders(fmts,options={})
       Array(fmts).each do |format|
         Array(options[:for]).each do |o| 
@@ -70,6 +106,8 @@ module Ruport
       end
     end
     
+    # Gives a list of formats registered for this plugin.
+    # (but not which renderers they're registered on)
     def self.formats
       @formats ||= []
     end
@@ -91,6 +129,14 @@ module Ruport
       @output.replace("")
     end
 
+    # Provides a shortcut for per format handlers.
+    #
+    # Example:
+    #
+    #
+    #   # will only be called if formatter is called for html output
+    #   html { output << "Look, I'm handling html" }
+    #
     def method_missing(id,*args)
       if self.class.formats.include?(id)
         yield() if format == id
