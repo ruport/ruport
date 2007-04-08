fd1f30835cd2510d37adc45b124c696b0c788e09
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-04-08 23:15:57 +0000

    Rudimentary Record#to_a bench
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@716 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

---
+++ util/bench/data/record/bench_to_a.rb
@@ -0,0 +1,22 @@
+require "ruport"
+require "benchmark"
+
+large = (1..1000).to_a
+large_attributes = large.map { |e| e.to_s.reverse }
+
+large_record = Ruport::Data::Record.new(large,
+                                        :attributes => large_attributes)
+
+small_record = Ruport::Data::Record.new([1,2,3],
+                                        :attributes => %w[a b c])
+
+Benchmark.bm do |x|
+  SMALL_N = 100000
+  LARGE_N = 1000
+  x.report("to_a : Large Record") {
+    LARGE_N.times { large_record.to_a }
+  }
+  x.report("to_a: Small Record") {
+    SMALL_N.times { small_record.to_a }
+  }
+end
