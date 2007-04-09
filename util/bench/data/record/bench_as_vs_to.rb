8c0ac4c8589d3d26b259be58f057df5cf36070ee
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-04-09 14:51:01 +0000

    Reorder benchmark
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@719 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

---
+++ util/bench/data/record/bench_as_vs_to.rb
@@ -0,0 +1,22 @@
+require "ruport"
+require "benchmark"
+
+class MyFormat < Ruport::Formatter;
+  renders :nothing, :for => Ruport::Renderer::Row
+end
+
+record = Ruport::Data::Record.new [1,2,3]
+
+Benchmark.bm do |x|
+
+  N = 10000
+  
+  x.report("as(:nothing)") do
+    N.times { record.as(:nothing) }
+  end
+
+  x.report("to_nothing") do
+    N.times { record.to_nothing }
+  end
+
+end
