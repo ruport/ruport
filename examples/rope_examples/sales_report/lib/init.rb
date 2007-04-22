95395fa20b5b9d8eaf9433696ae1aa95f6ff66c4
mikem836 <mikem836@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-04-22 16:45:04 +0000

    update rope sales report example
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@825 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- examples/rope_examples/sales_report/lib/init.rb
+++ examples/rope_examples/sales_report/lib/init.rb
@@ -1,9 +1,39 @@
 begin
   require "rubygems"
-  gem "ruport","=0.8.0"
+  gem "ruport","=0.9.6"
 rescue LoadError 
   nil
 end
 require "ruport"
 require "lib/helpers"
-require "config/ruport_config"
+require "config/environment"
+
+class String
+  def /(other)
+   self + "/" + other
+  end
+end
+
+class Ruport::Report
+  
+  def output_dir
+    config.output_dir or dir('output')
+  end
+
+  def data_dir
+    config.data_dir or dir('data')
+  end
+
+  def query_dir
+    config.query_dir or dir('sql')
+  end
+
+  def template_dir
+    config.template_dir or dir('templates')
+  end
+
+  private
+  def dir(name)
+    "/Users/mikem836/sales_report/#{name}"
+  end
+end
