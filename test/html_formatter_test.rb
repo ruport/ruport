1ca4a1a74579ee2bcbd9fb4dc840ae9e82678922
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-05-06 18:35:07 +0000

    Fix for #298, minor changes
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@894 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- test/html_formatter_test.rb
+++ test/html_formatter_test.rb
@@ -1,13 +1,7 @@
 require "test/unit"
 require "ruport"
 
-begin
-  require "rubygems"
-rescue LoadError
-  nil
-end
-
-class TestFormatHTML < Test::Unit::TestCase
+class TestRenderHTMLTable < Test::Unit::TestCase
   
   def test_html_table
     a = Ruport::Formatter::HTML.new
@@ -37,13 +31,21 @@ class TestFormatHTML < Test::Unit::TestCase
       "\n\t\t\t<td>4</td>\n\t\t\t<td>5</td>\n\t\t\t<td>6</td>\n\t"+
       "\t</tr>\n\t</table>",actual)   
     
-  end
+  end   
+end
+   
 
+class TestRenderHTMLRow < Test::Unit::TestCase
+  
   def test_render_html_row
     actual = Ruport::Renderer::Row.render_html { |r| r.data = [1,2,3] }
     assert_equal("\t\t<tr>\n\t\t\t<td>1</td>\n\t\t\t<td>2"+
                  "</td>\n\t\t\t<td>3</td>\n\t\t</tr>\n",actual)
   end
+end
+   
+
+class TestRenderHTMLGroup < Test::Unit::TestCase
     
   def test_render_html_group
     group = Ruport::Data::Group.new(:name => 'test',
@@ -68,7 +70,11 @@ class TestFormatHTML < Test::Unit::TestCase
       "\n\t\t\t<td>2</td>\n\t\t\t<td>3</td>\n\t\t</tr>\n\t\t<tr>"+
       "\n\t\t\t<td>4</td>\n\t\t\t<td>5</td>\n\t\t\t<td>6</td>\n\t"+
       "\t</tr>\n\t</table>", actual
-  end
+  end                                           
+end
+
+
+class TestRenderHTMLGrouping < Test::Unit::TestCase
 
   def test_render_html_grouping
     table = Table(%w[a b c]) << [1,2,3] << [1,1,3] << [2,7,9]
@@ -95,6 +101,15 @@ class TestFormatHTML < Test::Unit::TestCase
                  "\t<th>c</th>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<td>7</td>\n"+
                  "\t\t\t<td>9</td>\n\t\t</tr>\n\t</table>\n", actual
   end
+end  
+     
+
+class TestHTMLFormatterHelpers < Test::Unit::TestCase
+  begin
+    require "rubygems"
+  rescue LoadError
+    nil
+  end
   
   def test_textile     
     require "redcloth"
@@ -103,5 +118,4 @@ class TestFormatHTML < Test::Unit::TestCase
   rescue LoadError
     STDERR.puts "Skipping textile test... needs redcloth"
   end
-
 end
