13695eebee5291e6cb960b5f04a6294d2505bf0f
mikem836 <mikem836@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-05-07 02:54:19 +0000

    refactor grouping tests, #297
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@909 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- test/grouping_test.rb
+++ test/grouping_test.rb
@@ -4,6 +4,12 @@ begin; require "rubygems"; rescue LoadError; nil; end
 
 class TestGroup < Test::Unit::TestCase
 
+  def setup
+    @group = Ruport::Data::Group.new(:name => 'test',
+                                     :data => [[1,2,3]],
+                                     :column_names => %w[a b c])
+  end
+  
   def test_group_constructor
     group = Ruport::Data::Group.new(:name => 'test',
                                     :data => [[1,2,3]],
@@ -15,11 +21,8 @@ class TestGroup < Test::Unit::TestCase
   end
 
   def test_should_copy_group
-    group = Ruport::Data::Group.new(:name => 'test',
-                                    :data => [[1,2,3]],
-                                    :column_names => %w[a b c])
-    group.create_subgroups("a")
-    copy = group.dup
+    @group.create_subgroups("a")
+    copy = @group.dup
     assert_equal 'test', copy.name
     assert_equal Ruport::Data::Record.new([1,2,3],:attributes => %w[a b c]),
       copy.data[0]
@@ -31,73 +34,79 @@ class TestGroup < Test::Unit::TestCase
     assert_equal b, copy.subgroups
   end
 
-  def test_group_as
-    group = Ruport::Data::Group.new(:name => 'test',
-                                    :data => [%w[Ruport Is Sexy]],
-                                    :column_names => %w[Software Isnt Sexy])
-    assert_equal(7,group.to_text.to_a.length)
-    assert_equal(5,group.as(:text, :show_table_headers => false).to_a.length)
-    assert_equal(13,group.to_html.to_a.length)
-  end
-
   def test_eql
-    group = Ruport::Data::Group.new(:name => 'test',
-                                    :data => [%w[ruport is sexy]],
-                                    :column_names => %w[software isnt sexy])
-    table = [%w[ruport is sexy]].to_table(%w[software isnt sexy])
+    table = [[1,2,3]].to_table(%w[a b c])
 
     group2 = Ruport::Data::Group.new(:name => 'test',
-                                     :data => [%w[ruport is sexy]],
-                                     :column_names => %w[software isnt sexy])
+                                     :data => [[1,2,3]],
+                                     :column_names => %w[a b c])
 
-    assert_raises(NoMethodError) { group == table }
-    assert_equal group, group2
-    assert_equal group, group.dup
+    assert_raises(NoMethodError) { @group == table }
+    assert_equal @group, group2
+    assert_equal @group, @group.dup
   end
   
   def test_create_subgroups
-    group = Ruport::Data::Group.new(:name => 'test',
-                                    :data => [[1,2,3],[4,5,6]],
-                                    :column_names => %w[a b c])
+    group = @group << [4,5,6]
     group.create_subgroups("a")
     b = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 ),
+                                        :column_names => %w[b c],
+                                        :name => 1 ),
           4 => Ruport::Data::Group.new( :data => [[5,6]],
-                                  :column_names => %w[b c],
-                                  :name => 4 ) }
+                                        :column_names => %w[b c],
+                                        :name => 4 ) }
     assert_equal b, group.subgroups
+    
+    group.create_subgroups("b")
+    c = { 2 => Ruport::Data::Group.new( :data => [[3]],
+                                        :column_names => %w[c],
+                                        :name => 2 ) }
+    d = { 5 => Ruport::Data::Group.new( :data => [[6]],
+                                        :column_names => %w[c],
+                                        :name => 5 ) }
+    assert_equal c, group.subgroups[1].subgroups
+    assert_equal d, group.subgroups[4].subgroups
   end    
   
   def test_grouped_data
-    a = Group(nil, :data => [[1,2,3],[4,5,6]],
-              :column_names => %w[a b c]).send(:grouped_data, "a")
+    a = @group << [4,5,6]
     b = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 ),
+                                        :column_names => %w[b c],
+                                        :name => 1 ),
           4 => Ruport::Data::Group.new( :data => [[5,6]],
-                                  :column_names => %w[b c],
-                                  :name => 4 ) }
-    assert_equal b, a
+                                        :column_names => %w[b c],
+                                        :name => 4 ) }
+    assert_equal b, a.send(:grouped_data, "a")
   end
   
   def test_kernel_hack
-    expected = Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 )
-    assert_equal expected, Group(1, :data => [[2,3]],
-                                    :column_names => %w[b c]) 
-  end      
+    assert_equal @group, Group('test', :data => [[1,2,3]],
+                                       :column_names => %w[a b c]) 
+  end
+end
 
+class TestGroupRendering < Test::Unit::TestCase
+
+  def setup
+    @group = Ruport::Data::Group.new(:name => 'test',
+                                     :data => [[1,2,3]],
+                                     :column_names => %w[a b c])
+  end
+
+  def test_group_as
+    assert_equal(7, @group.to_text.to_a.length)
+    assert_equal(5, @group.as(:text,
+                              :show_table_headers => false).to_a.length)
+    assert_equal(13, @group.to_html.to_a.length)
+  end
+  
   def test_as_throws_proper_errors
-    group = Ruport::Data::Group.new(:name => 'test',
-                                    :data => [[1,2,3]],
-                                    :column_names => %w[a b c])
- 
-    assert_nothing_raised { group.as(:csv) }
-    assert_nothing_raised { group.to_csv }
-    assert_raises(Ruport::Renderer::UnknownFormatError) { group.as(:nothing) }
-    assert_raises(Ruport::Renderer::UnknownFormatError) { group.to_nothing }
+    assert_nothing_raised { @group.as(:csv) }
+    assert_nothing_raised { @group.to_csv }
+    assert_raises(Ruport::Renderer::UnknownFormatError) {
+      @group.as(:nothing) }
+    assert_raises(Ruport::Renderer::UnknownFormatError) {
+      @group.to_nothing }
   end
 
   class MyGroupSub < Ruport::Data::Group; end
@@ -106,42 +115,16 @@ class TestGroup < Test::Unit::TestCase
     t = MyGroupSub.new(:column_names => %w[b c],:name => "1") << [2,3]
     assert_equal "1\n\nb,c\n2,3\n", t.to_csv
   end
- 
-  
 end
-
+    
 class TestGrouping < Test::Unit::TestCase
   
+  def setup
+    table = [[1,2,3],[4,5,6]].to_table(%w[a b c])
+    @grouping = Ruport::Data::Grouping.new(table, :by => "a")
+  end
+  
   def test_grouping_constructor
-    a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
-    b = Ruport::Data::Grouping.new(a, :by => "a")
-    c = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 ),
-         4 => Ruport::Data::Group.new( :data => [[5,6]],
-                                  :column_names => %w[b c],
-                                  :name => 4 ) }
-    assert_equal c, b.data
-  end      
-  
-  def test_grouping_indexing   
-    a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
-    b = Ruport::Data::Grouping.new(a, :by => "a")
-    c = [Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 ),
-         Ruport::Data::Group.new( :data => [[5,6]],
-                                  :column_names => %w[b c],
-                                  :name => 4 ),    
-         Ruport::Data::Group.new( :data => [],
-                                  :column_names => %w[b c],
-                                  :name => 2)]
-    assert_equal c[0], b[1] 
-    assert_equal c[1], b[4]   
-    assert_raises(IndexError) { b[2] }                 
-  end    
-  
-  def test_should_copy_grouping
     a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
     b = Ruport::Data::Grouping.new(a, :by => "a")
     c = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
@@ -150,69 +133,75 @@ class TestGrouping < Test::Unit::TestCase
           4 => Ruport::Data::Group.new( :data => [[5,6]],
                                         :column_names => %w[b c],
                                         :name => 4 ) }
-    copy = b.dup
-    assert_equal c, copy.data
+    assert_equal c, b.data
+  end      
+  
+  def test_grouping_indexing
+    a = [Ruport::Data::Group.new( :data => [[2,3]],
+                                  :column_names => %w[b c],
+                                  :name => 1 ),
+         Ruport::Data::Group.new( :data => [[5,6]],
+                                  :column_names => %w[b c],
+                                  :name => 4 ),
+         Ruport::Data::Group.new( :data => [],
+                                  :column_names => %w[b c],
+                                  :name => 2)]
+    assert_equal a[0], @grouping[1]
+    assert_equal a[1], @grouping[4]
+    assert_raises(IndexError) { @grouping[2] }
+  end    
+  
+  def test_should_copy_grouping
+    a = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
+                                        :column_names => %w[b c],
+                                        :name => 1 ),
+          4 => Ruport::Data::Group.new( :data => [[5,6]],
+                                        :column_names => %w[b c],
+                                        :name => 4 ) }
+    copy = @grouping.dup
+    assert_equal a, copy.data
     assert_equal "a", copy.grouped_by
   end
 
   def test_append
    a =[[1,2,3],[4,5,6]].to_table(%w[a b c])
-   b = Ruport::Data::Grouping.new(a, :by => "a")
-   c = [Ruport::Data::Group.new( :data => [[2,3]],
-                                 :column_names => %w[b c],
-                                 :name => 1 ),
-        Ruport::Data::Group.new( :data => [[5,6]],
-                                 :column_names => %w[b c],
-                                 :name => 4 )]   
-   b << a.to_group("hand banana")
-   assert_equal b["hand banana"], a.to_group("hand banana")   
+   @grouping << a.to_group("red snapper")
+   assert_equal @grouping["red snapper"], a.to_group("red snapper")
    
-   assert_raises(ArgumentError) { b << a.to_group("hand banana") }   
-  end          
-  
-  def test_grouping_as
-    table = Ruport::Data::Table.new(:data => [%w[Ruport Is Sexy]],
-                                    :column_names => %w[Software Isnt Sexy])
-    grouping = Ruport::Data::Grouping.new(table, :by => 'Software')
-    assert_equal(8,grouping.to_text.to_a.length)
-    assert_equal(6,grouping.as(:text, :show_table_headers => false).to_a.length)
+   assert_raises(ArgumentError) { @grouping << a.to_group("red snapper") }
   end
-
+  
   def test_grouped_by
-    a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
-    b = Grouping(a, :by => "a")
-    assert_equal "a", b.grouped_by
+    assert_equal "a", @grouping.grouped_by
   end
 
   def test_kernel_hack
-    a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
-    b = Grouping(a, :by => "a")
-    c = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
-                                  :column_names => %w[b c],
-                                  :name => 1 ),
+    a = { 1 => Ruport::Data::Group.new( :data => [[2,3]],
+                                        :column_names => %w[b c],
+                                        :name => 1 ),
           4 => Ruport::Data::Group.new( :data => [[5,6]],
-                                  :column_names => %w[b c],
-                                  :name => 4 ) }
-    assert_equal c, b.data
+                                        :column_names => %w[b c],
+                                        :name => 4 ) }
+    assert_equal a, @grouping.data
   end   
 
   def test_grouping_on_multiple_columns
     a = [[1,2,3,4],[4,5,6,7]].to_table(%w[a b c d])
     b = Ruport::Data::Grouping.new(a, :by => %w[a b c])
     c = { 1 => Ruport::Data::Group.new( :data => [[2,3,4]],
-                                  :column_names => %w[b c d],
-                                  :name => 1 ),
+                                        :column_names => %w[b c d],
+                                        :name => 1 ),
           4 => Ruport::Data::Group.new( :data => [[5,6,7]],
-                                  :column_names => %w[b c d],
-                                  :name => 4 ) }
+                                        :column_names => %w[b c d],
+                                        :name => 4 ) }
     assert_equal c, b.data
 
     d = { 2 => Ruport::Data::Group.new( :data => [[3,4]],
-                                  :column_names => %w[c d],
-                                  :name => 2 ) }
+                                        :column_names => %w[c d],
+                                        :name => 2 ) }
     e = { 5 => Ruport::Data::Group.new( :data => [[6,7]],
-                                  :column_names => %w[c d],
-                                  :name => 5 ) }
+                                        :column_names => %w[c d],
+                                        :name => 5 ) }
     assert_equal d, b[1].subgroups
     assert_equal e, b[4].subgroups
   end      
@@ -231,18 +220,6 @@ class TestGrouping < Test::Unit::TestCase
     assert_equal %w[schweet], sub.column("id")
   end
   
-  def test_as_throws_proper_errors
-
-    a = [[1,2,3],[4,5,6]].to_table(%w[a b c])
-    b = Grouping(a, :by => "a")
- 
- 
-    assert_nothing_raised { b.as(:csv) }
-    assert_nothing_raised { b.to_csv }
-    assert_raises(Ruport::Renderer::UnknownFormatError) { b.as(:nothing) }
-    assert_raises(Ruport::Renderer::UnknownFormatError) { b.to_nothing }
-  end   
-  
   class TicketStatus < Ruport::Data::Record
 
     def closed
@@ -255,9 +232,8 @@ class TestGrouping < Test::Unit::TestCase
 
   end
   
-  def test_grouping_summary  
-
-    source = Table("test/samples/ticket_count.csv", 
+  def test_grouping_summary
+    source = Table("test/samples/ticket_count.csv",
                      :record_class => TicketStatus)
     grouping = Grouping(source,:by => "date")
     
@@ -266,14 +242,14 @@ class TestGrouping < Test::Unit::TestCase
       opened = group.sigma { |r| r.opened  }
       closed = group.sigma { |r| r.closed  }
       expected << { :date => date, :opened => opened, :closed => closed }
-    end       
+    end
     
     actual = grouping.summary :date,
       :opened => lambda { |g| g.sigma(:opened) },
       :closed => lambda { |g| g.sigma(:closed) },
       :order => [:date,:opened,:closed]
       
-    assert_equal expected, actual     
+    assert_equal expected, actual
     
     actual = grouping.summary :date,
       :opened => lambda { |g| g.sigma(:opened) },
@@ -282,6 +258,8 @@ class TestGrouping < Test::Unit::TestCase
     assert_equal [], expected.column_names - actual.column_names
   end   
  
+  class MyRecord < Ruport::Data::Record; end
+  
   def test_grouping_should_set_record_class
     a = Table(%w[a b c], :record_class => MyRecord) { |t| 
           t << [1,2,3]
@@ -298,8 +276,27 @@ class TestGrouping < Test::Unit::TestCase
     a = MyGroupingSub.new(t, :by => "a") 
     assert_equal "1\n\nb,c\n2,3\n\n", a.to_csv
   end
-  
 end
 
-class MyRecord < Ruport::Data::Record
+class TestGroupingRendering < Test::Unit::TestCase
+
+  def setup
+    table = [[1,2,3],[4,5,6]].to_table(%w[a b c])
+    @grouping = Ruport::Data::Grouping.new(table, :by => "a")
+  end
+  
+  def test_grouping_as
+    assert_equal(16, @grouping.to_text.to_a.length)
+    assert_equal(12, @grouping.as(:text,
+      :show_table_headers => false).to_a.length)
+  end
+
+  def test_as_throws_proper_errors
+    assert_nothing_raised { @grouping.as(:csv) }
+    assert_nothing_raised { @grouping.to_csv }
+    assert_raises(Ruport::Renderer::UnknownFormatError) {
+      @grouping.as(:nothing) }
+    assert_raises(Ruport::Renderer::UnknownFormatError) {
+      @grouping.to_nothing }
+  end
 end
