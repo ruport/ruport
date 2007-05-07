39fba024e621736129fc4495412b73635e764ae7
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-05-07 00:26:27 +0000

    SQL split test renaming
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@903 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- test/sql_split_test.rb
+++ test/sql_split_test.rb
@@ -2,17 +2,18 @@ require "test/unit"
 require "ruport"
 class TestSqlSplit < Test::Unit::TestCase
 	include Ruport
+ 	
+	def test_sql_split_trivial
+		sql = "SELECT * FROM ruport_test"
+		split = Query::SqlSplit.new sql
+		assert_equal( 1, split.size )
+		assert_equal( sql, split.first )
+	end  
 	
-	def test_sql_split1
+	def test_sql_split_complex
 		sql = File.read 'test/samples/ruport_test.sql'
 		split = Query::SqlSplit.new sql
 		assert_equal( 'SELECT * FROM ruport_test', split.last )
 	end
 	
-	def test_sql_split2
-		sql = "SELECT * FROM ruport_test"
-		split = Query::SqlSplit.new sql
-		assert_equal( 1, split.size )
-		assert_equal( sql, split.first )
-	end
 end
