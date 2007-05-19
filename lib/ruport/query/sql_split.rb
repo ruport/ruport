501b6c12b1247faedd6855334bac9f026b090a83
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-05-19 13:28:25 +0000

    tabs should be illegal
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@1025 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport/query/sql_split.rb
+++ lib/ruport/query/sql_split.rb
@@ -13,21 +13,21 @@ module Ruport
     # This class properly splits up multi-statement SQL input for use with
     # Ruby/DBI
     class SqlSplit < Array  #:nodoc:
-			def initialize( sql )
-				super()
-				next_sql = ''
-				sql.each do |line|
-					unless line =~ /^--/ or line =~ %r{^/\*.*\*/;} or line =~ /^\s*$/
-						next_sql << line
-						if line =~ /;$/
-							next_sql.gsub!( /;\s$/, '' )
-							self << next_sql
-							next_sql = ''
-						end
-					end
-				end
-				self << next_sql if next_sql != ''
-			end
-		end
+      def initialize( sql )
+        super()
+        next_sql = ''
+        sql.each do |line|
+          unless line =~ /^--/ or line =~ %r{^/\*.*\*/;} or line =~ /^\s*$/
+            next_sql << line
+            if line =~ /;$/
+              next_sql.gsub!( /;\s$/, '' )
+              self << next_sql
+              next_sql = ''
+            end
+          end
+        end
+        self << next_sql if next_sql != ''
+      end
+    end
   end
 end
