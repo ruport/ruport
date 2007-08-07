692381b41f435bd188a515fa638e54769814bf46
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-08-07 19:28:10 +0000

    Solaris compatible terminal_size
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@1144 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- lib/ruport.rb
+++ lib/ruport.rb
@@ -70,8 +70,13 @@ module Ruport #:nodoc:#
     rescue LoadError             # If we're not on Windows try...
       # A Unix savvy method to fetch the console columns, and rows.
       def terminal_size
-        size = `stty size 2>/dev/null`.split.map { |x| x.to_i }.reverse
-        return $? == 0 ? size : [80,24]
+       if /solaris/ =~ RUBY_PLATFORM
+         output = `stty`
+         [output.match('columns = (\d+)')[1].to_i,
+          output.match('rows = (\d+)')[1].to_i]
+       else
+          `stty size`.split.map { |x| x.to_i }.reverse
+        end
       end
 
    end
