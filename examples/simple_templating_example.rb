fad9808d0de7df7840075587851a9846d2f3d31e
sandal <sandal@bb2e8eb0-7117-0410-aac4-c024b40ed5f7>
2007-08-29 02:11:42 +0000

    Update to templating example
    git-svn-id: http://stonecode.svnrepository.com/svn/ruport/ruport/trunk@1174 bb2e8eb0-7117-0410-aac4-c024b40ed5f7

--- examples/simple_templating_example.rb
+++ examples/simple_templating_example.rb
@@ -14,7 +14,9 @@ Ruport::Formatter::Template.create(:simple) do |t|
   }
   t.column_format = {
     :alignment => :center,
-    :heading => { :justification => :right }
+  }                       
+  t.heading_format = {
+    :alignment => :right
   }
   t.grouping_format = {
     :style => :separated
@@ -22,29 +24,11 @@ Ruport::Formatter::Template.create(:simple) do |t|
 end
 
 Ruport::Formatter::Template.create(:derived, :base => :simple) do |t|
-  t.table_format[:font_size] = 12
+  t.table_format[:show_headings] = true
 end
-
-# Uncomment this section to use the :simple2 template
-#
-# Ruport::Formatter::Template.create(:simple2) do |t|
-#   t.page_layout = :portrait
-#   t.grouping_style = :offset
-# end  
-# 
-# class Ruport::Formatter::PDF
-#   
-#   def apply_template
-#     options.paper_orientation = template.page_layout
-#     options.style = template.grouping_style
-#   end
-#   
-# end 
 
 t = Table(%w[a b c]) << [1,2,3] << [1,"hello",6] << [2,3,4] 
 g = Grouping(t, :by => "a")
 
 puts g.to_pdf(:template => :simple)
 #puts g.to_pdf(:template => :derived)
-#puts g.to_pdf(:template => :simple2)
-#puts g.to_pdf
