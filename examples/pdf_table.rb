cf5dc321e7c890aa3ba32602ace7f30deee2b523
Michael Pope <map7777@gmail.com>
2010-04-18 00:56:41 +1000

    Add minimal Prawn example.

--- examples/pdf_table.rb
+++ examples/pdf_table.rb
@@ -1,5 +1,26 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
 require "ruport"
 
 class Document < Ruport::Controller
+  stage :body
 
+  def setup
+    self.data = Ruport::Table(
+      :column_names => %w(Make Model Year),
+      :data => [
+        %w(Nissan Skyline 1989),
+        %w(Mercedes-Benz 500SL 2005),
+        %w(Kia Sinatra 2008)
+       ])
+  end
 end
+
+class DocumentFormatter < Ruport::Formatter::PrawnPDF
+  renders :prawn_pdf, :for => Document
+
+  def build_body
+    draw_table(data)
+  end
+end
+
+Document.render(:prawn_pdf, :file => 'prawn_pdf.pdf')
