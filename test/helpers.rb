0b4e55ad113a3a9da7a48cec6e5c0a08bbd8070f
Keenan Brock <keenan@thebrocks.net>
2020-06-05 20:54:32 -0400

    remove rubygems from tests
    this is no longer needed

--- test/helpers.rb
+++ test/helpers.rb
@@ -16,12 +16,8 @@ end
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'ruport'
-begin; require 'rubygems'; rescue LoadError; nil; end
-
-
 require 'minitest'
 require 'minitest/autorun'
-
 require 'minitest/spec'
 require 'minitest/unit'
 require 'shoulda-context'
